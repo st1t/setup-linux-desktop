6cd60fa55aa9abb71c646235d6e74932eb238c13
Shota Ito <shota.ito.jp@gmail.com>
2020-06-07 21:49:47 +0900

    Fix pyenv init

--- ubuntu-20.04/install-pyenv.sh
+++ ubuntu-20.04/install-pyenv.sh
@@ -5,7 +5,7 @@
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 exec "$SHELL"
 
 sudo apt-get update
