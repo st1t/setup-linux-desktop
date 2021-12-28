0653d385131dfb2eb9b933b785afe0924246336d
Shota Ito <shota.ito.jp@gmail.com>
2021-12-28 16:26:40 +0900

    Update install-rbenv.sh

--- ubuntu-20.04/install-rbenv.sh
+++ ubuntu-20.04/install-rbenv.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
+sudo apt update
+sudo apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
+
+
 # open https://github.com/rbenv/rbenv#basic-github-checkout
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
